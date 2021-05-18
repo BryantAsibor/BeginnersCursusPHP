fc9c963995728c387d8d328bb22a6c9d759ec285
BryantAsibor <2136149@talnet.nl>
2021-05-18 18:06:56 +0200

    update

---
+++ H03/index.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>PHP Opdrachten</title>
+</head>
+<body>
+<?php
+
+//    for($i = 1; $i<=10 ;$i++) {
+//        echo "<img src='img/lion".$i.".jpg'>";
+//    }
+
+$i = 1;
+while($i <= 10) {
+    echo "<img src='img/lion".$i.".jpg'>";
+    $i++;
+}
+
+    ?>
+</body>
+</html>
